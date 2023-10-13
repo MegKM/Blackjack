dd319472593a7d395dff14dc59ef0bdfc805d38a
Megan M <megankmithen@gmail.com>
2023-10-13 13:44:02 +1100

    initial layout

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/*Then you will receive two cards from the dealer. 
+The dealer’s also dealt two cards, with one of them face-up so you can see what it is. 
+In blackjack games, an ace can be worth 1 or 11, while number cards are worth their face value and face cards 
+(jack, queen and king), are all worth 10.
+
+If your first two cards add up to 21, i.e. you get an ace and a card worth 10, 
+you’ll win the game, so long as the dealer doesn’t have a 21 also. 
+If the dealer’s first hand is worth 21 and yours isn’t, you’ll lose.
+
+There are several courses of action you can take if your hand doesn’t add up to 21:
+
+–           Hit. You receive an additional card.
+
+–           Stand. You receive no more cards and keep your hand as it is.
+
+–           Double. Double you bet, take one more card and stand
+
+If your hand ever goes over 21 – you have bust and have lost that hand. 
+If the dealer busts and you don’t, you win. Once you’ve acted, it’s the dealer’s turn. 
+The dealer can’t decide what to do; instead, the dealer has to stick to rules depending on 
+what the value of their hand is. In most games, the dealer has to hit on a soft 17, in others 
+the dealer has to stand on this hand.
+
+A soft hand is any line-up of cards containing an ace. 
+If you have a soft hand, you can draw an additional card without going bust. 
+This is because you can have the ace count as 1 instead of 11. 
+A hard hand is any hand that doesn’t contain  ace, or a hand with ace that has to be counted as 1. 
+Otherwise you’d go bust. For example, a hand containing a 6, and 8 and an ace is hard: if the ace is 11, 
+your total would be 25 and you’d be out; the ace, therefore, has to count as 1, bringing your total to 15. */
+
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
