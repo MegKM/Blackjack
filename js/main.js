b796b716b1d096d11cf96df372b4ce9e6f54cb25
Megan M <megankmithen@gmail.com>
2023-10-15 09:15:46 +1100

    Result messages added

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-/*Then you will receive two cards from the dealer. 
-The dealer’s also dealt two cards, with one of them face-up so you can see what it is. 
-In blackjack games, an ace can be worth 1 or 11, while number cards are worth their face value and face cards 
-(jack, queen and king), are all worth 10.
-
-If your first two cards add up to 21, i.e. you get an ace and a card worth 10, 
-you’ll win the game, so long as the dealer doesn’t have a 21 also. 
-If the dealer’s first hand is worth 21 and yours isn’t, you’ll lose.
-
-There are several courses of action you can take if your hand doesn’t add up to 21:
-
-–           Hit. You receive an additional card.
-
-–           Stand. You receive no more cards and keep your hand as it is.
-
-–           Double. Double you bet, take one more card and stand
-
-If your hand ever goes over 21 – you have bust and have lost that hand. 
-If the dealer busts and you don’t, you win. Once you’ve acted, it’s the dealer’s turn. 
-The dealer can’t decide what to do; instead, the dealer has to stick to rules depending on 
-what the value of their hand is. In most games, the dealer has to hit on a soft 17, in others 
-the dealer has to stand on this hand.
-
-A soft hand is any line-up of cards containing an ace. 
-If you have a soft hand, you can draw an additional card without going bust. 
-This is because you can have the ace count as 1 instead of 11. 
-A hard hand is any hand that doesn’t contain  ace, or a hand with ace that has to be counted as 1. 
-Otherwise you’d go bust. For example, a hand containing a 6, and 8 and an ace is hard: if the ace is 11, 
-your total would be 25 and you’d be out; the ace, therefore, has to count as 1, bringing your total to 15. */
 
 /*----- constants -----*/
 const suits = ['s', 'c', 'd', 'h'];
@@ -39,7 +10,6 @@ let cards = '';
 
 // might need to add to function to call on init
 
-let totalCardsPlayed = 4;
 const playersHand = [];
 const dealersHand = [];
 let playerHasBlackjack = false;
@@ -47,6 +17,7 @@ let dealerHasBlackjack = false;
 const dealerRestsAmount = 17;
 let dealersRunningTotal = 0;
 let playersRunningTotal = 0;
+let totalCardsPlayed = 0;
 
 /*----- cached elements  -----*/
 const buttonElements = {
@@ -88,6 +59,13 @@ const playersCards = {
     fifth: document.getElementById('player-card5'),
 }
  
+const displayElements = {
+    dealersCardTotalDisplay: document.getElementById('dealers-card-total-display'),
+    playersCardTotalDisplay: document.getElementById('players-card-total-display'),
+    dealersResult: document.getElementById('dealers-result'),
+    playersResult: document.getElementById('players-result'),
+    gameResult: document.getElementById('game-result'),
+}
 
 /*----- event listeners -----*/
 function activateBettingEventListerners(){
@@ -194,53 +172,71 @@ function dealCards(){
     removeBettingEventListerners();
     activateGameplayEventListerners();
 
-    playersCards.first.setAttribute("class", `card large ${cards[0].face}`);
-    playersHand.push(cards[0].value);
-    dealersCards.first.setAttribute("class", `card large ${cards[1].face}`);
-    dealersHand.push(cards[1].value);
-    playersCards.second.setAttribute("class", `card large ${cards[2].face}`);
-    playersHand.push(cards[2].value);
+    setTimeout(() => {    
+        playersCards.first.setAttribute("class", `card large ${cards[totalCardsPlayed].face}`);
+        playersHand.push(cards[totalCardsPlayed].value);
+        totalCardsPlayed += 1;
+    }, 250);
 
-    let cardSuit = cards[3].face.slice(0,1);
+    setTimeout(() => {
+        dealersCards.first.setAttribute("class", `card large ${cards[totalCardsPlayed].face}`);
+        dealersHand.push(cards[totalCardsPlayed].value);
+        totalCardsPlayed += 1;
+    }, 500);
+
+    setTimeout(() => {
+        playersCards.second.setAttribute("class", `card large ${cards[totalCardsPlayed].face}`);
+        playersHand.push(cards[totalCardsPlayed].value);
+        totalCardsPlayed += 1;
+    }, 750);
+
+
+    let cardSuit = cards[totalCardsPlayed].face.slice(0,1);
     if(cardSuit === 's' || cardSuit === 'c'){
         cardSuit = "back-blue"
     }else{
         cardSuit= "back-red"
     }
 
-    dealersCards.second.setAttribute("class", `card large ${cardSuit}`);
-    dealersHand.push(cards[3].value);
-    console.log(playersHand);
-    console.log(dealersHand);
+    setTimeout(() => {
+        dealersCards.second.setAttribute("class", `card large ${cardSuit}`);
+        dealersHand.push(cards[totalCardsPlayed].value);
+        totalCardsPlayed += 1;
+    }, 1000);
 
     dealersRunningTotal = dealersHand.reduce((acc, num) => acc + num);
     playersRunningTotal = playersHand.reduce((acc, num) => acc + num);
+    updatePlayersOnscreenScore();
     dealerHasBlackjack = checkIfBlackjack(dealersHand);
     playerHasBlackjack = checkIfBlackjack(playersHand);
     if (playerHasBlackjack && dealerHasBlackjack){
         roundOver();
     }
     if(playerHasBlackjack){
+        displayElements.playersResult.innerText = "You have blackjack!";
         dealerTakesCard();
     };
 }
 
 function playerTakesCard(){
     if (playersCards.third.getAttribute("class") === "empty"){
-        playersCards.third.setAttribute("class", `card large ${cards[4].face}`)
-        playersRunningTotal += cards[4].value;
+        playersCards.third.setAttribute("class", `card large ${cards[totalCardsPlayed].face}`)
+        playersRunningTotal += cards[totalCardsPlayed].value;
+        updatePlayersOnscreenScore();
         totalCardsPlayed += 1 
         if(checkIfPlayerBust()) {roundOver()};
     }
     else if (playersCards.fourth.getAttribute("class") === "empty"){
-        playersCards.fourth.setAttribute("class", `card large ${cards[5].face}`)
-        playersRunningTotal += cards[5].value;
+        playersCards.fourth.setAttribute("class", `card large ${cards[totalCardsPlayed].face}`)
+        playersRunningTotal += cards[totalCardsPlayed].value;
+        updatePlayersOnscreenScore();
         totalCardsPlayed += 1 
         if(checkIfPlayerBust()) {roundOver()};
     }
     else if (playersCards.fifth.getAttribute("class") === "empty"){
-        playersCards.fifth.setAttribute("class", `card large ${cards[6].face}`)
-        playersRunningTotal += cards[6].value;
+        playersCards.fifth.setAttribute("class", `card large ${cards[totalCardsPlayed].face}`)
+        playersRunningTotal += cards[totalCardsPlayed].value;
+        updatePlayersOnscreenScore();
         totalCardsPlayed += 1 
         if(checkIfPlayerBust()) {roundOver()};
     }
@@ -251,6 +247,7 @@ function playerTakesCard(){
 
 function dealerTakesCard(){
     dealersCards.second.setAttribute("class", `card large ${cards[3].face}`)
+    updateDealersOnscreenScore();
     console.log(dealersHand)
     if(dealerHasBlackjack){
         roundOver();
@@ -259,14 +256,17 @@ function dealerTakesCard(){
     if(dealersRunningTotal < dealerRestsAmount){
         dealersCards.third.setAttribute("class", `card large ${cards[totalCardsPlayed].face}`);
         dealersRunningTotal += cards[totalCardsPlayed].value;
+        updateDealersOnscreenScore();
         totalCardsPlayed += 1;
         if(dealersRunningTotal < dealerRestsAmount){
             dealersCards.fourth.setAttribute("class", `card large ${cards[totalCardsPlayed].face}`)
             dealersRunningTotal += cards[totalCardsPlayed].value;
+            updateDealersOnscreenScore();
             totalCardsPlayed += 1
             if(dealersRunningTotal < dealerRestsAmount){
                 dealersCards.fifth.setAttribute("class", `card large ${cards[totalCardsPlayed].face}`)
                 dealersRunningTotal += cards[totalCardsPlayed].value;
+                updateDealersOnscreenScore();
                 roundOver();
             }
             else{
@@ -305,19 +305,37 @@ function roundOver(){
     console.log("player: ", playersRunningTotal);
     console.log("dealer: ", dealersRunningTotal);
 
-    if(playersRunningTotal > dealersRunningTotal || dealersRunningTotal > 21){
+    if(playersRunningTotal > 21){
+        displayElements.playersResult.innerText = "You bust!";
+        displayElements.gameResult.innerText = "Dealer wins.";
+    }
+    else if(dealersRunningTotal > 21){
+        displayElements.dealersResult.innerText = "Dealer bust";
+        displayElements.gameResult.innerText = "You win!";
         currentBank += currentPot * 2;
         currentPot = 0;
+        moneyElements.potElement.innerText = currentPot;
+        moneyElements.bankElement.innerText = currentBank;
+    }    
+    else if(playersRunningTotal > dealersRunningTotal){
+        currentBank += currentPot * 2;
+        currentPot = 0;
+        displayElements.dealersResult.innerHTML = `Dealer rests on ${dealersRunningTotal}`;
+        displayElements.playersResult.innerText = "You win!";
         moneyElements.potElement.innerText = currentPot;
         moneyElements.bankElement.innerText = currentBank;
     }
     else if(dealersRunningTotal > playersRunningTotal){
         currentPot = 0;
+        displayElements.playersResult.innerHTML = `You rested on ${playersRunningTotal}`;
+        displayElements.dealersResult.innerHTML = `Dealer has ${dealersRunningTotal}`;
+        displayElements.gameResult.innerText = "Dealer wins.";
         moneyElements.potElement.innerText = currentPot;
     }
     else{
         currentBank += currentPot;
         currentPot = 0;
+        displayElements.gameResult.innerText = "It's a tie.";
         moneyElements.potElement.innerText = currentPot;
         moneyElements.bankElement.innerText = currentBank;
     }    
@@ -361,6 +379,14 @@ function playerStands(){
     dealerTakesCard();
 }
 
+function updatePlayersOnscreenScore(){
+    displayElements.playersCardTotalDisplay.innerText = playersRunningTotal;
+};
+
+function updateDealersOnscreenScore(){
+    displayElements.dealersCardTotalDisplay.innerText = dealersRunningTotal;
+}
+
 function playAgain(){
     playersHand.length = 0;
     dealersHand.length = 0;
@@ -368,6 +394,12 @@ function playAgain(){
     dealerHasBlackjack = false;
     dealersRunningTotal = 0;
     playersRunningTotal = 0;
+    totalCardsPlayed = 0;
+    displayElements.dealersCardTotalDisplay.innerText = "";
+    displayElements.playersCardTotalDisplay.innerText = "";
+    displayElements.dealersResult.innerText = "";
+    displayElements.playersResult.innerText = "";
+    displayElements.gameResult.innerText = "";
 
     playersCards.first.setAttribute("class", "empty");
     playersCards.second.setAttribute("class", "empty");
